f0446d2bb8babb8c9b6c42b9c5509df2469a2472
3laa-96 <3laa.xvx@gmail.com>
2022-12-06 14:11:43 +0200

    v1.2

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 let count=0
 let countEl = document.getElementById('numberOfPeople');
+let saveEl = document.getElementById('save-el');
 function increment() {
     count += 1;
     countEl.innerText = count;
  
 }
 function save() {
-    console.log(count);
+    
+    saveEl.innerText += "  "+count +" - ";
     
 }
