2dfd73d53467a3577b3812d385727d6cd43199f3
3laa-96 <3laa.xvx@gmail.com>
2022-12-06 15:11:59 +0200

    v1.4

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function increment() {
  
 }
 function save() {
-    
-    saveEl.innerText += "  "+count +" - ";
+    countEl.innerHTML = 0;
+    saveEl.textContent += "  " + count + " -";
+    count = 0;
     
 }
